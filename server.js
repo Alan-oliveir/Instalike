7375e3d01322e70fc999b154ace57141fc1d676f
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2024-11-20 16:47:23 -0300

    refactor posts API: separate routes, controllers, and models for improved structure

--- server.js
+++ server.js
@@ -1,33 +1,10 @@
 import express from "express";
-import conectarAoBanco from "./src/config/dbConfig.js";
-
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json());
+
+routes(app);
 
 app.listen(4000, () => {
   console.log("Server is running on http://localhost:4000");
-});
-
-async function getTodosPosts() {
-  const db = conexao.db("imersao-instabytes");
-  const colecao = db.collection("posts");
-  return colecao.find().toArray();
-}
-
-app.get("/posts", async (req, res) => {
-  const posts = await getTodosPosts();
-  res.status(200).json(posts);
-});
-
-function getPostById(id) {
-  return posts.findIndex((post) => {
-    return post.id === Number(id);
-  });
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = getPostById(req.params.id);
-  res.status(200).json(posts[index]);
 });
